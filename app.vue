664a6ad8184a32f9b80fa013d21b3a82f0f2d37d
yaaKo081v <MccaddenBenell@gmail.com>
2025-06-19 13:26:18 +0100

    修复了 登录失败 问题

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NConfigProvider inline-theme-disabled>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </NConfigProvider>
+</template>
+<script setup>
+import {
+  NConfigProvider
+} from "naive-ui"
+</script>
